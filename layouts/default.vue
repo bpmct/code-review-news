1f3a72b3d9aebdb213a0ed1060bef92ebabcf018
Ben Potter <me@bpmct.net>
2020-04-14 11:48:55 -0700

    Full path fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -39,6 +39,7 @@ export default {
           property: 'og:image',
           content:
             (process.env.URL ? process.env.URL : '') +
+            '_nuxt' +
             this.$store.state.featureImage
         },
         {
